da0ba6d96a83ef31f831e5dfbcf978146c26ca8f
naixsu <plnacua02@gmail.com>
2024-02-17 18:13:42 +0800

    Modified tiles

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -14,7 +14,17 @@ var tile : Vector2
 var tilemapTiles = {
 	"grass": 0,
 	"hover": 1,
-	"tilled": 2
+	"tilled": 2,
+	"grassTiles": 3,
+	"tilledTiles": 4,
+	"plantTiles": 5
+}
+
+var tilemapLayers = {
+	"grass": 0,
+	"hover": 1,
+	"tilled": 2,
+	"plant": 3,
 }
 
 func _ready():
@@ -43,16 +53,17 @@ func set_tile(pos: Vector2, type: String):
 		
 	var tileObj = tileDict[str(pos)]
 	
-	if type == "tilled":
+	if type == "tilled": # till tile
 		tileObj.isTilled = true
-		set_cell(0, pos, tilemapTiles["tilled"], Vector2i(0, 0), 0)
+		set_cell(tilemapLayers["tilled"], pos, tilemapTiles["tilledTiles"], Vector2i(1, 1), 0)
 	elif type == "seed":
 		tileObj.isSeed = true
 	elif type == "grow":
 		tileObj.isGrown = true
-	elif type == "grass":
+	elif type == "grass": # untill tile
 		if tileObj.isTilled and not tileObj.isSeed:
-			set_cell(0, pos, tilemapTiles["grass"], Vector2i(0, 0), 0)
+			erase_cell(tilemapLayers["tilled"], tile)
+			tileObj.isTilled = false
 	
 	print(tileObj)
 
@@ -75,18 +86,19 @@ func erase_tile(layer: int, pos: Vector2):
 func hover_tile():
 	if tileDict.has(str(tile)):
 		var tileObj = tileDict[str(tile)]
-		if not tileObj.isSeed:
+		if not tileObj.isSeed and not tileObj.isOuter:
 			set_cell(1, tile, tilemapTiles["hover"], Vector2i(0, 0), 0)
 
 
-func init_set_tile(pos: Vector2):
+func init_set_tile(pos: Vector2, atlas: Vector2i, isOuter: bool):
 	tileDict[str(pos)] = {
 		"pos": pos,
+		"isOuter": isOuter,
 		"isSeed": false,
 		"isGrown": false,
 		"isTilled": false
 	}
-	set_cell(0, pos, tilemapTiles["grass"], Vector2i(0, 0), 0)
+	set_cell(0, pos, tilemapTiles["grassTiles"], atlas, 0)
 
 
 func init():
@@ -97,5 +109,47 @@ func init():
 				continue
 			
 			var pos = Vector2(col, row)
+			var atlas = Vector2i(1, 1)
+			var isOuter = false
 			
-			init_set_tile(pos)
+			if row == 1:
+				if col == 1:
+					atlas = Vector2i(0, 0)
+					isOuter = true
+				elif col == 18:
+					atlas = Vector2i(2, 0)
+					isOuter = true
+				else:
+					atlas = Vector2i(1, 0)
+					isOuter = true
+			
+			elif row == 8:
+				if col == 1:
+					atlas = Vector2i(0, 2)
+					isOuter = true
+				elif col == 18:
+					atlas = Vector2i(2, 2)
+					isOuter = true
+				else:
+					atlas = Vector2i(1, 2)
+					isOuter = true
+			
+			if col == 1:
+				if row >= 2 and row <= 7:
+					atlas = Vector2i(0, 1)
+					isOuter = true
+					
+			elif col == 18:
+				if row >= 2 and row <= 7:
+					atlas = Vector2i(2, 1)
+					isOuter = true
+				
+			if not isOuter:
+				if randf() < 0.25:
+					#  chance of generating random coordinates 
+					# in the ranges (0, 5) to (5, 5) or (0, 6) to (5, 6)"
+					var ranX = randi() % 6
+					var ranY = randi() % 2 + 5
+					atlas = Vector2i(ranX, ranY)
+
+			init_set_tile(pos, atlas, isOuter)
