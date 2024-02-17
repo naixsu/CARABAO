ae408365ce3c4e0325d166a9d811416fc66eff12
naixsu <plnacua02@gmail.com>
2024-02-17 14:12:24 +0800

    Change tile on click

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -9,6 +9,14 @@ var cols = 20
 
 var tile : Vector2
 
+# This var is dependent on the tile IDs in TileMap
+# DO NOT CHANGE
+var tilemapTiles = {
+	"grass": 0,
+	"hover": 1,
+	"tilled": 2
+}
+
 func _ready():
 	# Init
 	# Godot does cols first
@@ -18,12 +26,37 @@ func _ready():
 func _process(_delta):
 	tile = local_to_map(get_global_mouse_position())
 
-	erase_tiles()
+	erase_hover_tiles()
 	hover_tile()
 
 
+func _input(event):
+	# Click to till
+	if event.is_action_pressed("Click"):
+		set_tile(tile, "tilled")
+	if event.is_action_pressed("RClick"):
+		set_tile(tile, "grass")
 
-func erase_tiles():
+func set_tile(pos: Vector2, type: String):
+	if not tileDict.has(str(tile)):
+		return
+		
+	var tileObj = tileDict[str(pos)]
+	
+	if type == "tilled":
+		tileObj.isTilled = true
+		set_cell(0, pos, tilemapTiles["tilled"], Vector2i(0, 0), 0)
+	elif type == "seed":
+		tileObj.isSeed = true
+	elif type == "grow":
+		tileObj.isGrown = true
+	elif type == "grass":
+		if tileObj.isTilled and not tileObj.isSeed:
+			set_cell(0, pos, tilemapTiles["grass"], Vector2i(0, 0), 0)
+	
+	print(tileObj)
+
+func erase_hover_tiles():
 	for col in cols:
 		for row in rows:
 			if col == 0 or col == cols - 1\
@@ -34,24 +67,26 @@ func erase_tiles():
 			
 			erase_tile(1, pos)
 
+
 func erase_tile(layer: int, pos: Vector2):
 	erase_cell(layer, pos)
+
 
 func hover_tile():
 	if tileDict.has(str(tile)):
 		var tileObj = tileDict[str(tile)]
-		if not tileObj.hasSeed or not tileObj.tilled:
-			set_cell(1, tile, 1, Vector2i(0, 0), 0)
+		if not tileObj.isSeed:
+			set_cell(1, tile, tilemapTiles["hover"], Vector2i(0, 0), 0)
 
 
 func init_set_tile(pos: Vector2):
 	tileDict[str(pos)] = {
-		"type": "grass",
-		"hasSeed": false,
-		"fullGrown": false,
-		"tilled": false
+		"pos": pos,
+		"isSeed": false,
+		"isGrown": false,
+		"isTilled": false
 	}
-	set_cell(0, pos, 0, Vector2i(0, 0), 0)
+	set_cell(0, pos, tilemapTiles["grass"], Vector2i(0, 0), 0)
 
 
 func init():
